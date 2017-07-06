6395ae0fd04549598873d63e41c0a880f62fb9c2
Heqiliang <heqiliang@weplus.cn>
2017-07-06 11:25:53 +0800

    init program

---
+++ HQLFullCollapseTextLabel/HQLFullCollapseTextLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQLFullCollapseTextLabel
+//
+//  Created by weplus on 2017/7/6.
+//  Copyright © 2017年 weplus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HQLFullCollapseTextLabel/HQLFullCollapseTextLabel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HQLFullCollapseTextLabel
+//
+//  Created by weplus on 2017/7/6.
+//  Copyright © 2017年 weplus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
